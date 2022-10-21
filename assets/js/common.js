ae3ffbfde8b7d56637258e84632139e4c13a0385
Dgley Ramos <dgleysilva@gmail.com>
2022-10-21 15:31:18 -0300

    Menu toggle mobile and tablet

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,36 @@
 /*=============== Show Menu =============== */
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== Remove Menu Mobile =============== */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu');
+
+  // when we click on each link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction))
+
 
 /*=============== Background Header =============== */
 function scrollHeader(){
@@ -72,7 +96,7 @@ send.addEventListener('click', (e) => {
     contactEmail.value = '';
     Message.value = '';
   }
-})
+});
 
 /*=============== Style Switcher =============== */
 
