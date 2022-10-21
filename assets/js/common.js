f81af22537be2e8fbdd1fa66d04b4b0184aac6a9
Dgley Ramos <dgleysilva@gmail.com>
2022-10-21 15:19:46 -0300

    Aplicando responsividade

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,7 +9,70 @@
 /*=============== Remove Menu Mobile =============== */
 
 /*=============== Background Header =============== */
+function scrollHeader(){
+  const header = document.getElementById('header');
+  // when the scroll is greater than 50 viewport height, add scrooll-header class to header tag
+  if(this.scrollY >=50){
+    header.classList.add('scroll-header');
+  }else{
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== Contact Form =============== */
+const contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      Message = document.getElementById('message'),
+      contactMessage = document.getElementById('contact-message'),
+      send = document.getElementById('send');
+
+
+send.addEventListener('click', (e) => {
+  e.preventDefault();
+
+
+  // Check if field has a value
+  if(
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    Message.value === ''
+  ){
+    // add and remove color
+    contactMessage.classList.remove('color-light');
+    contactMessage.classList.add('color-dark');
+
+
+    //show message
+    contactMessage.textContent = 'Preencha todos os campos'
+  } else {
+    // serviceID - tempateID - #form - publickey
+    emailjs.sendForm(
+      'service_hhr09gw',
+      'template_zddbav3',
+      '#contact-form',
+      '8z13Bcb-5v9YrUDOS'
+    ).then(() => {
+      // Show message and add color
+      contactMessage.classList.add('color-light');
+      contactMessage.textContent = 'Mensagem enviada! ✔️';
+
+      // remove message after 5 seconds
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      }, 5000);
+    },
+    (error) => {
+      alert('OOPs! ALGO DEU ERRADO...', error)
+    });
+
+    // clear input fields
+    contactName.value = '';
+    contactEmail.value = '';
+    Message.value = '';
+  }
+})
 
 /*=============== Style Switcher =============== */
+
