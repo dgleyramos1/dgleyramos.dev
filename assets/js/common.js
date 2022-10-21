bdcdd9f112283ac710e8b910708a1834bea51563
Dgley Ramos <dgleysilva@gmail.com>
2022-10-21 17:06:56 -0300

    Adicionando um switcher para mudar  tema

--- assets/js/common.js
+++ assets/js/common.js
@@ -99,4 +99,28 @@ send.addEventListener('click', (e) => {
 });
 
 /*=============== Style Switcher =============== */
+const styleSwitcherToggle = document.querySelector('.style__switcher-toggler'),
+      styleSwitcher = document.querySelector('.style__switcher');
 
+styleSwitcherToggle.addEventListener('click', () => {
+  styleSwitcher.classList.toggle('open');
+})
+// hide switcher on scroll
+window.addEventListener('scroll', () => {
+  if(styleSwitcher.classList.contains('open')){
+    styleSwitcher.classList.remove('open')
+  }
+});
+
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color){
+  alternateStyles.forEach((style) => {
+    if(color === style.getAttribute('title')){
+      style.removeAttribute('disabled');
+    } else {
+      style.setAttribute('disabled', 'true')
+    }
+  })
+}
